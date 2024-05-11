04389ec30bded516a5ecb26e319e80636a2677c3
AnonymousAcid <anonymousacid14@gmail.com>
2024-05-10 17:41:48 -0700

    Create default app and spawn cube

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use bevy::math;
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, test_system)
+        .run();
+}
+
+fn test_system(
+    mut commands : Commands,
+    mut meshes: Assets<Mesh>,
+    mut materials: Assets<StandardMaterial>
+) {
+    let cube = Cuboid {
+        half_size: Vec3::new(1.,1.,1.)
+    };
+    let material = Color::rgb(1., 0., 0.);
+
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(cube.mesh()),
+        material: materials.add(material.into()),
+        ..default()
+    });
 }
